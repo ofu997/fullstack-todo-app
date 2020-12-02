7c548a1b9745cc2f0e8ee66e879e1ef3ea9af8ec
Michael Ray Estepa <mrestepa1@gmail.com>
2020-12-02 12:31:03 +0800

    Attempt at login authentication (bugged)

---
+++ client/src/views/auth/LoginView.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import { Button, TextField, Typography, Container } from "@material-ui/core";
+
+export class LoginView extends Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+  handleUsernameChange = (event) => {
+    this.setState({ username: event.target.value });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleFormSubmit = async (event) => {
+    // Login User
+    event.preventDefault();
+
+    await fetch(`/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      }),
+      redirect: "follow",
+    }).then((response) => {
+      if (response.status === 201) {
+        console.log(response.status);
+        window.location.replace("/ddd");
+      }
+    });
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+  };
+
+  render() {
+    return (
+      <Container
+        component="main"
+        maxWidth="xs"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: " 100vh",
+        }}
+      >
+        <div>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className="" noValidate onSubmit={this.handleFormSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoFocus
+              onChange={this.handleUsernameChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={this.handlePasswordChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className=""
+            >
+              Sign In
+            </Button>
+          </form>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default LoginView;
